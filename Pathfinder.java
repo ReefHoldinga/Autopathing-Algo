b7d8a96d13b3ccbc7d4e96e329348fd6bad71e10
Reef Holdinga <reefholdinga@gmail.com>
2024-01-12 22:09:02 -0500

    Miner can now spawn on any 4 walls

--- Pathfinder.java
+++ Pathfinder.java
@@ -32,8 +32,18 @@ public class Pathfinder {
       }
     }
 
-    // Generate rocks
+    // Generate 3 rocks
+    for (int i = 0; i < 3; i++) {
+      generateRock(rand, width, height, matrix);
+    }
 
+    // Generate Miner
+    generateMiner(rand, width, height, matrix);
+
+    return matrix;
+  }
+
+  private static void generateRock(Random rand, int width, int height, int[][] matrix) {
     int rockStartX = rand.nextInt(width - 1);
     int rockStartY = rand.nextInt(height - 1);
 
@@ -45,40 +55,37 @@ public class Pathfinder {
         }
       }
     }
+    // return matrix;
+  }
 
-    rockStartX = rand.nextInt(width - 1);
-    rockStartY = rand.nextInt(height - 1);
+  private static void generateMiner(Random rand, int width, int height, int[][] matrix) {
 
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 2; j++) {
+    // left vs right or top vs down; zero means left or up, one means right or down
+    int position = rand.nextInt(2);
 
-        if (matrix[i][j] != 1) {
-          matrix[rockStartX + i][rockStartY + j] = 2;
-        }
-      }
+    // generate numbers to determine if we are doing right left or up down
+    int num1 = rand.nextInt(width);
+    int num2 = rand.nextInt(height);
+
+    // determine coordinates of miners, this takes care of if minerX != 0 and
+    // position == 1
+    int minerX = (num1 > num2) ? num1 : 0;
+    int minerY = 0;
+
+    if (minerX == 0 && position == 0) {
+      minerY = num2;
+    } else if (minerX == 0 && position == 1) {
+      minerY = num2;
+      minerX = width - 1;
+    } else if (minerX != 0 && position == 1) {
+      minerY = height - 1;
     }
 
-    rockStartX = rand.nextInt(width - 1);
-    rockStartY = rand.nextInt(height - 1);
-
-    for (int i = 0; i < 2; i++) {
-      for (int j = 0; j < 2; j++) {
-
-        if (matrix[rockStartX + i][rockStartY + j] != 1) {
-          matrix[rockStartX + i][rockStartY + j] = 2;
-        }
-      }
+    if (matrix[minerY][minerX] == 0) {
+      matrix[minerY][minerX] = 3;
+    } else {
+      generateMiner(rand, width, height, matrix);
     }
-
-    // Generate Miner
-    int minerX = rand.nextInt(width);
-    int minerY = rand.nextInt(height);
-
-    if (matrix[minerX][minerY] == 0) {
-      matrix[minerX][minerY] = 3;
-    }
-
-    return matrix;
   }
 
   public static void displayMatrix(int[][] matrix) {
